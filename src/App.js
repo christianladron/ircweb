75b4fdab347a53ceed4dd4dd2cf100764145d267
christian ladron de guevara reyes <christian.ladron@gmail.com>
2017-08-23 00:21:51 -0500

    Añadido login

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import Chat from './Chat.js';
 class App extends Component {
 	constructor(props){
 		super(props);
-		this.state = {messages:[{channel:"chat 1",user:"Christian",message:"Hola mundo",time:new Date()}],connections:[
-			{user:"Christian",channel:"chat 1"},
-			{user:"Roberto",channel:"chat 2"},
-			{user:"Christian",channel:"chat 2"},
-			{user:"Roberto",channel:"chat 3"},
-			{user:"Christian",channel:"chat 4"}
-		]};
+		let initialConnection = {user:"",channel:""};
+		this.state = {messages:[],connections:[ initialConnection],selectedTab:initialConnection};
 		this.pushMessage = this.pushMessage.bind(this);
+		this.login = this.login.bind(this);
 	}
 	pushMessage(mes){
 		this.setState({messages:this.state.messages.concat(mes)});
+	}
+	login(creds){
+		let index = this.state.connections.map((o)=>{return JSON.stringify(o)}).indexOf(JSON.stringify(creds));
+		if(index === -1 ) this.setState({messages:this.state.messages.concat({user:"admin",channel:creds.channel,message:(creds.user+" joined the channel"),time:new Date()}),selectedTab:creds,connections:this.state.connections.slice(0,-1).concat(creds)});
+		else  this.setState({selectedTab:this.state.connections[index]});
 	}
 	render() {
 		let cons = this.state.connections;
@@ -27,15 +28,17 @@ class App extends Component {
 			let messages=this.state.messages.filter((b)=>{
 				return o.channel===b.channel;
 			});
-			return (<Chat key={JSON.stringify(o)} pushMessage={this.pushMessage} messages={messages} app={this}  chat={o} selectedTab={this.state.selectedTab} />)
+			return (<Chat key={JSON.stringify(o)} pushMessage={this.pushMessage} messages={messages} app={this}  chat={o} selectedTab={this.state.selectedTab} login={this.login}/>)
 		});
 		return (
 			<div className="App">
 			<header>
 			{tabs}
 			<div id="addTab" onClick={()=>{
-				if(cons.length === 0 || (cons[cons.length - 1].channel !== ""))
-				this.setState({connections:this.state.connections.concat({user:"",channel:""})})
+				if(cons.length === 0 || (cons[cons.length - 1].channel !== "")){
+					let newConnection = {user:"",channel:""};
+					this.setState({connections:this.state.connections.concat(newConnection),selectedTab:newConnection})
+				}
 			}}><i className="fa fa-plus" aria-hidden="true"></i></div>
 			<div style={{float:"unset"}}></div>
 			</header>
--- src/Chat.js
+++ src/Chat.js
@@ -4,10 +4,11 @@ import Message from './Message.js';
 class Chat extends Component {
 	constructor(props){
 		super(props);
-		this.state = {newMessage:""};
+		this.state = {newMessage:"",user:"",pwd:"",channel:""};
 	this.createMessage = this.createMessage.bind(this);
 	this.write = this.write.bind(this);
 	this.pushMessage = this.pushMessage.bind(this);
+	this.login = this.login.bind(this);
 	}
 	write(e){
 		if(e.target.value!=="\n") this.setState({newMessage:e.target.value});
@@ -22,9 +23,16 @@ class Chat extends Component {
 	createMessage(o){
 		return <Message messageUser={o.user} time={o.time} key={o.user+o.time} connectionUser={this.props.chat.user} message={o.message}/>;
 	}
+	login(){
+		let user = this.state.user;
+		let channel = this.state.channel;
+		let pwd = this.state.pwd;
+		if (user !== "" && channel !== "" && pwd !== "" && user !== "admin") this.props.login({user,channel});
+	}
 	render() {
 		let messages = this.props.messages.map(this.createMessage);
 		let newMessage = this.state.newMessage;
+		if(this.props.chat.user !=="" && this.props.chat.channel !==""){
 		return ( <div className="chat-container" style={{"display":(this.props.selectedTab===this.props.chat)?"block":"none"}}>
 				<div className="chat-text">
 				{messages}
@@ -34,6 +42,14 @@ class Chat extends Component {
 					<div className="send-message" onClick={this.pushMessage}><p>Send</p></div>
 				</div>
 			</div>)
+		}else{
+			return <form onKeyPress={(e)=>{if(e.charCode === 13) this.login()}} className="chat-login" style={{"display":(this.props.selectedTab===this.props.chat)?"block":"none"}}>
+				<div><label htmlFor="channel">Channel</label><input value={this.state.channel} onChange={(e)=>{this.setState({channel:e.target.value})}} id="channel" type="text"/></div>
+				<div><label htmlFor="user">Username</label><input value={this.state.user} onChange={(e)=>{this.setState({user:e.target.value})}} id="user" type="text"/></div>
+				<div><label htmlFor="pwd">Password</label><input value={this.state.pwd} onChange={(e)=>{this.setState({pwd:e.target.value})}} id="pwd" type="password"/></div>
+				<div><input type="button" onClick={this.login} value="login" id="login" /></div>
+				</form>
+		}
 	}
 }
 
